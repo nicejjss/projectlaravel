f3fba3645062edb3d17da0b6895769d9b625254b
nicejjss <locdaoduc20022gmail.com>
2023-03-17 11:10:11 +0700

    Sua model Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,14 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
-    protected $table = 'products';
-    public $timestamps= false;
 
-    public function category(){
-        return $this->belongsTo(Category::class,'category_id','id');
+    protected $table = 'products';
+    public $timestamps = false;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'id');
+    }
 
     public function orders()
     {
-        return $this->belongsToMany(Order::class,'order_details','product_id','order_id')->withPivot('number');
+        return $this->belongsToMany(Order::class, 'order_details', 'product_id', 'order_id')->withPivot('number');
     }
 }
